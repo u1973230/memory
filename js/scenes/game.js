f4450b8b56390188824de6edfed2841b674789b0
u1973230 <u1973230@campus.udg.edu>
2022-04-23 18:39:22 +0200

    hdgfh

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -30,8 +30,7 @@ class GameScene extends Phaser.Scene {
 		this.add.image(350, 300, arraycards[1]);
 		this.add.image(450, 300, arraycards[2]);
 		this.add.image(550, 300, arraycards[3]);
-		this.add.image(650, 300, arraycards[4]);
-		this.add.image(750, 300, arraycards[5]);
+		
 
 
 		this.cards = this.physics.add.staticGroup();
@@ -41,8 +40,7 @@ class GameScene extends Phaser.Scene {
 		this.cards.create(350, 300, 'back');
 		this.cards.create(450, 300, 'back');
 		this.cards.create(550, 300, 'back');
-		this.cards.create(650, 300, 'back');
-		this.cards.create(750, 300, 'back');
+		
 
 		
 		
