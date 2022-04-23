e3bc883d8447b684650701c57319bad101179576
u1973230 <u1973230@campus.udg.edu>
2022-04-23 19:14:44 +0200

    vxcb

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -23,7 +23,7 @@ class GameScene extends Phaser.Scene {
 		var json = localStorage.getItem("config") || '{"cards":2,"dificulty":"hard"}';
 		var options_data = JSON.parse(json);
 		var cartas = options_data.cards*2;
-		var vector_cartas = arraycards.slice(0,cartas);
+		//var vector_cartas = arraycards.slice(0,cartas);
 		this.cameras.main.setBackgroundColor(0xBFFCFF);
 
 		
@@ -45,7 +45,7 @@ class GameScene extends Phaser.Scene {
 		
 		for(var k = 4;  k < cartas ; k++){
 
-			this.add.image(125*k+550,300,vector_cartas[k]);
+			this.add.image(125*k+550,300,arraycards[k]);
 		}
 		
 	
